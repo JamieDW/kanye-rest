4ba4ec068c08429bbcab0dc440aea01a63726e9c
Jamie Whelpton <jamie.whelpton94@gmail.com>
2024-01-11 17:22:29 +0000

    Add feature tests for QuoteController and TokenController

--- tests/Feature/Http/Controllers/Api/QuoteControllerTest.php
+++ tests/Feature/Http/Controllers/Api/QuoteControllerTest.php
@@ -7,25 +7,26 @@ use Tests\TestCase;
 
 class QuoteControllerTest extends TestCase
 {
+
     public function test_quotes_index_returns_a_successful_response_when_authenticated(): void
     {
-        $response = $this->get(route('quotes.index'));
+        $response = $this->get(route('quotes.index'), $this->getAuthenticatedHeader());
 
         $response->assertStatus(Response::HTTP_OK);
     }
 
     public function test_quotes_new_returns_a_successful_response_when_authenticated(): void
     {
-        $response = $this->get(route('quotes.new'));
+        $response = $this->get(route('quotes.new'), $this->getAuthenticatedHeader());
 
         $response->assertStatus(Response::HTTP_OK);
     }
 
-    public function test_quotes_purge_returns_a_successful_response_when_authenticated(): void
+    public function test_quotes_purge_returns_a_no_content_response_when_authenticated(): void
     {
-        $response = $this->get(route('quotes.purge'));
+        $response = $this->get(route('quotes.purge'), $this->getAuthenticatedHeader());
 
-        $response->assertStatus(Response::HTTP_OK);
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
     }
 
     public function test_quotes_index_returns_a_unsuccessful_response_when_unauthenticated(): void
@@ -47,5 +48,14 @@ class QuoteControllerTest extends TestCase
         $response = $this->get(route('quotes.purge'));
 
         $response->assertStatus(Response::HTTP_UNAUTHORIZED);
+    }
+
+    public function getAuthenticatedHeader(): array {
+
+        $token = $this->get(route('token.generate'))['token'];
+
+        return [
+            'Authorization' => 'Bearer ' . $token
+        ];
     }
 }
--- tests/Feature/Http/Controllers/Api/TokenControllerTest.php
+++ tests/Feature/Http/Controllers/Api/TokenControllerTest.php
@@ -16,4 +16,24 @@ class TokenControllerTest extends TestCase
 
         $response->assertStatus(Response::HTTP_OK);
     }
+
+    public function test_token_endpoint_returns_a_token(): void
+    {
+        $response = $this->get(route('token.generate'));
+
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertJsonStructure(['token']);
+    }
+
+    public function test_token_endpoint_returns_the_same_token_if_authenticated(): void
+    {
+        $originalToken = $this->get(route('token.generate'))['token'];
+
+        $response = $this->get(route('token.generate'), [
+            'Authorization' => 'Bearer ' . $originalToken
+        ]);
+
+        $this->assertEquals($originalToken, $response['token']);
+    }
 }
